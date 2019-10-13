4200c8ab2cb8924bd4035fc9f40fea32eca9263d
Kelvin Lee <klee561@my.bcit.ca>
2019-10-13 00:58:27 -0700

    listed streets of containers using json

--- app/src/main/java/com/example/theworld/MainActivity.java
+++ app/src/main/java/com/example/theworld/MainActivity.java
@@ -2,13 +2,81 @@ package com.example.theworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<String> opLocations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset(getApplicationContext()));
+            JSONArray litterBinsArray = obj.getJSONArray("features");
+            String result = "";
+            for(int i = 0; i < litterBinsArray.length(); i++) {
+                result = litterBinsArray.getJSONObject(i).getJSONObject("properties").getString("OPERATING_LOCATION");
+                opLocations.add(result);
+            }
+            String locations = "";
+            for(int i = 0; i < opLocations.size(); i++) {
+                locations += opLocations.get(i) + "\n\n";
+            }
+            TextView testView = findViewById(R.id.testView);
+            testView.setText(locations);
+
+
+        } catch (final JSONException e) {
+            Log.e(TAG, "Json parsing error: " + e.getMessage());
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Json parsing error: " + e.getMessage(),
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            });
+        }
+    }
+
+    private String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("litter_containers.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 }
