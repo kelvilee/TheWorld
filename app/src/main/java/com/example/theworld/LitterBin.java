6f8d4be7dd7b5d217d65eecdf720aa40f4166e23
Taylor <tlr.laan@gmail.com>
2019-10-15 17:02:40 -0700

    Basic LitterBin Class

---
+++ app/src/main/java/com/example/theworld/LitterBin.java
@@ -0,0 +1,550 @@
+package com.example.theworld;
+
+public class LitterBin
+{
+    private String LOCATION;
+
+    private String MOD_DATE;
+
+    private String WORK_ASSIGNMENT;
+
+    private String OPERATING_LOCATION;
+
+    private String SERIAL_NO;
+
+    private String INSERVICE_DATE;
+
+    private String COMMENTS;
+
+    private String BARCODE;
+
+    private String YRS_LIFE_EXPECTANCY;
+
+    private String type;
+
+    private String SERVICE_LEVEL;
+
+    private String MAKE;
+
+    private String SURFACE_TREATMENT;
+
+    private String CONTAINER_TYPE;
+
+    private String CRE_DATE;
+
+    private String STATUS;
+
+    private String OWNER;
+
+    private String DESCRIPTION;
+
+    private String STATUS_PARKS;
+
+    private String PARK_OWNERSHIP;
+
+    private String FACILITYID;
+
+    private String CONTAINER_NO;
+
+    private String YR;
+
+    private String OPERATING_LOCATION_TYPE;
+
+    private String GIS_ENTRY_STAGE;
+
+    private String CONTAINER_TYPE2;
+
+    private String MAP_PAGE;
+
+    private String PARK_LEGACY_ID;
+
+    private String INSERVICE_COST;
+
+    private String LEGACYID;
+
+    private String[] coordinates;
+
+    private String WARRANTY_PROVIDER;
+
+    private String STATUS_DATE;
+
+    private String MODEL;
+
+    private String ROAD_CODE;
+
+    private String LIFECYCLE_COST;
+
+    private String YTD_COST;
+
+    private String PROJECT_NO;
+
+    private String MAINTENANCE_RESPONSIBILITY;
+
+    private String VENDOR;
+
+    private String CONDITION;
+
+    private String WARRANTYDATE;
+
+    private String CONDITIONDATE;
+
+    private String OPERATIONAL_RESPONSIBILITY;
+
+    private String HOUSE_NO;
+
+    public String getLOCATION ()
+{
+    return LOCATION;
+}
+
+    public void setLOCATION (String LOCATION)
+    {
+        this.LOCATION = LOCATION;
+    }
+
+    public String getMOD_DATE ()
+    {
+        return MOD_DATE;
+    }
+
+    public void setMOD_DATE (String MOD_DATE)
+    {
+        this.MOD_DATE = MOD_DATE;
+    }
+
+    public String getWORK_ASSIGNMENT ()
+{
+    return WORK_ASSIGNMENT;
+}
+
+    public void setWORK_ASSIGNMENT (String WORK_ASSIGNMENT)
+    {
+        this.WORK_ASSIGNMENT = WORK_ASSIGNMENT;
+    }
+
+    public String getOPERATING_LOCATION ()
+{
+    return OPERATING_LOCATION;
+}
+
+    public void setOPERATING_LOCATION (String OPERATING_LOCATION)
+    {
+        this.OPERATING_LOCATION = OPERATING_LOCATION;
+    }
+
+    public String getSERIAL_NO ()
+{
+    return SERIAL_NO;
+}
+
+    public void setSERIAL_NO (String SERIAL_NO)
+    {
+        this.SERIAL_NO = SERIAL_NO;
+    }
+
+    public String getINSERVICE_DATE ()
+{
+    return INSERVICE_DATE;
+}
+
+    public void setINSERVICE_DATE (String INSERVICE_DATE)
+    {
+        this.INSERVICE_DATE = INSERVICE_DATE;
+    }
+
+    public String getCOMMENTS ()
+{
+    return COMMENTS;
+}
+
+    public void setCOMMENTS (String COMMENTS)
+    {
+        this.COMMENTS = COMMENTS;
+    }
+
+    public String getBARCODE ()
+{
+    return BARCODE;
+}
+
+    public void setBARCODE (String BARCODE)
+    {
+        this.BARCODE = BARCODE;
+    }
+
+    public String getYRS_LIFE_EXPECTANCY ()
+{
+    return YRS_LIFE_EXPECTANCY;
+}
+
+    public void setYRS_LIFE_EXPECTANCY (String YRS_LIFE_EXPECTANCY)
+    {
+        this.YRS_LIFE_EXPECTANCY = YRS_LIFE_EXPECTANCY;
+    }
+
+    public String getType ()
+    {
+        return type;
+    }
+
+    public void setType (String type)
+    {
+        this.type = type;
+    }
+
+    public String getSERVICE_LEVEL ()
+{
+    return SERVICE_LEVEL;
+}
+
+    public void setSERVICE_LEVEL (String SERVICE_LEVEL)
+    {
+        this.SERVICE_LEVEL = SERVICE_LEVEL;
+    }
+
+    public String getMAKE ()
+{
+    return MAKE;
+}
+
+    public void setMAKE (String MAKE)
+    {
+        this.MAKE = MAKE;
+    }
+
+    public String getSURFACE_TREATMENT ()
+{
+    return SURFACE_TREATMENT;
+}
+
+    public void setSURFACE_TREATMENT (String SURFACE_TREATMENT)
+    {
+        this.SURFACE_TREATMENT = SURFACE_TREATMENT;
+    }
+
+    public String getCONTAINER_TYPE ()
+    {
+        return CONTAINER_TYPE;
+    }
+
+    public void setCONTAINER_TYPE (String CONTAINER_TYPE)
+    {
+        this.CONTAINER_TYPE = CONTAINER_TYPE;
+    }
+
+    public String getCRE_DATE ()
+    {
+        return CRE_DATE;
+    }
+
+    public void setCRE_DATE (String CRE_DATE)
+    {
+        this.CRE_DATE = CRE_DATE;
+    }
+
+    public String getSTATUS ()
+{
+    return STATUS;
+}
+
+    public void setSTATUS (String STATUS)
+    {
+        this.STATUS = STATUS;
+    }
+
+    public String getOWNER ()
+    {
+        return OWNER;
+    }
+
+    public void setOWNER (String OWNER)
+    {
+        this.OWNER = OWNER;
+    }
+
+    public String getDESCRIPTION ()
+    {
+        return DESCRIPTION;
+    }
+
+    public void setDESCRIPTION (String DESCRIPTION)
+    {
+        this.DESCRIPTION = DESCRIPTION;
+    }
+
+    public String getSTATUS_PARKS ()
+{
+    return STATUS_PARKS;
+}
+
+    public void setSTATUS_PARKS (String STATUS_PARKS)
+    {
+        this.STATUS_PARKS = STATUS_PARKS;
+    }
+
+    public String getPARK_OWNERSHIP ()
+{
+    return PARK_OWNERSHIP;
+}
+
+    public void setPARK_OWNERSHIP (String PARK_OWNERSHIP)
+    {
+        this.PARK_OWNERSHIP = PARK_OWNERSHIP;
+    }
+
+    public String getFACILITYID ()
+    {
+        return FACILITYID;
+    }
+
+    public void setFACILITYID (String FACILITYID)
+    {
+        this.FACILITYID = FACILITYID;
+    }
+
+    public String getCONTAINER_NO ()
+{
+    return CONTAINER_NO;
+}
+
+    public void setCONTAINER_NO (String CONTAINER_NO)
+    {
+        this.CONTAINER_NO = CONTAINER_NO;
+    }
+
+    public String getYR ()
+{
+    return YR;
+}
+
+    public void setYR (String YR)
+    {
+        this.YR = YR;
+    }
+
+    public String getOPERATING_LOCATION_TYPE ()
+{
+    return OPERATING_LOCATION_TYPE;
+}
+
+    public void setOPERATING_LOCATION_TYPE (String OPERATING_LOCATION_TYPE)
+    {
+        this.OPERATING_LOCATION_TYPE = OPERATING_LOCATION_TYPE;
+    }
+
+    public String getGIS_ENTRY_STAGE ()
+{
+    return GIS_ENTRY_STAGE;
+}
+
+    public void setGIS_ENTRY_STAGE (String GIS_ENTRY_STAGE)
+    {
+        this.GIS_ENTRY_STAGE = GIS_ENTRY_STAGE;
+    }
+
+    public String getCONTAINER_TYPE2 ()
+    {
+        return CONTAINER_TYPE2;
+    }
+
+    public void setCONTAINER_TYPE2 (String CONTAINER_TYPE2)
+    {
+        this.CONTAINER_TYPE2 = CONTAINER_TYPE2;
+    }
+
+    public String getMAP_PAGE ()
+{
+    return MAP_PAGE;
+}
+
+    public void setMAP_PAGE (String MAP_PAGE)
+    {
+        this.MAP_PAGE = MAP_PAGE;
+    }
+
+    public String getPARK_LEGACY_ID ()
+{
+    return PARK_LEGACY_ID;
+}
+
+    public void setPARK_LEGACY_ID (String PARK_LEGACY_ID)
+    {
+        this.PARK_LEGACY_ID = PARK_LEGACY_ID;
+    }
+
+    public String getINSERVICE_COST ()
+{
+    return INSERVICE_COST;
+}
+
+    public void setINSERVICE_COST (String INSERVICE_COST)
+    {
+        this.INSERVICE_COST = INSERVICE_COST;
+    }
+
+    public String getLEGACYID ()
+{
+    return LEGACYID;
+}
+
+    public void setLEGACYID (String LEGACYID)
+    {
+        this.LEGACYID = LEGACYID;
+    }
+
+    public String[] getCoordinates ()
+    {
+        return coordinates;
+    }
+
+    public void setCoordinates (String[] coordinates)
+    {
+        this.coordinates = coordinates;
+    }
+
+    public String getWARRANTY_PROVIDER ()
+{
+    return WARRANTY_PROVIDER;
+}
+
+    public void setWARRANTY_PROVIDER (String WARRANTY_PROVIDER)
+    {
+        this.WARRANTY_PROVIDER = WARRANTY_PROVIDER;
+    }
+
+    public String getSTATUS_DATE ()
+{
+    return STATUS_DATE;
+}
+
+    public void setSTATUS_DATE (String STATUS_DATE)
+    {
+        this.STATUS_DATE = STATUS_DATE;
+    }
+
+    public String getMODEL ()
+{
+    return MODEL;
+}
+
+    public void setMODEL (String MODEL)
+    {
+        this.MODEL = MODEL;
+    }
+
+    public String getROAD_CODE ()
+{
+    return ROAD_CODE;
+}
+
+    public void setROAD_CODE (String ROAD_CODE)
+    {
+        this.ROAD_CODE = ROAD_CODE;
+    }
+
+    public String getLIFECYCLE_COST ()
+{
+    return LIFECYCLE_COST;
+}
+
+    public void setLIFECYCLE_COST (String LIFECYCLE_COST)
+    {
+        this.LIFECYCLE_COST = LIFECYCLE_COST;
+    }
+
+    public String getYTD_COST ()
+{
+    return YTD_COST;
+}
+
+    public void setYTD_COST (String YTD_COST)
+    {
+        this.YTD_COST = YTD_COST;
+    }
+
+    public String getPROJECT_NO ()
+{
+    return PROJECT_NO;
+}
+
+    public void setPROJECT_NO (String PROJECT_NO)
+    {
+        this.PROJECT_NO = PROJECT_NO;
+    }
+
+    public String getMAINTENANCE_RESPONSIBILITY ()
+{
+    return MAINTENANCE_RESPONSIBILITY;
+}
+
+    public void setMAINTENANCE_RESPONSIBILITY (String MAINTENANCE_RESPONSIBILITY)
+    {
+        this.MAINTENANCE_RESPONSIBILITY = MAINTENANCE_RESPONSIBILITY;
+    }
+
+    public String getVENDOR ()
+{
+    return VENDOR;
+}
+
+    public void setVENDOR (String VENDOR)
+    {
+        this.VENDOR = VENDOR;
+    }
+
+    public String getCONDITION ()
+{
+    return CONDITION;
+}
+
+    public void setCONDITION (String CONDITION)
+    {
+        this.CONDITION = CONDITION;
+    }
+
+    public String getWARRANTYDATE ()
+{
+    return WARRANTYDATE;
+}
+
+    public void setWARRANTYDATE (String WARRANTYDATE)
+    {
+        this.WARRANTYDATE = WARRANTYDATE;
+    }
+
+    public String getCONDITIONDATE ()
+{
+    return CONDITIONDATE;
+}
+
+    public void setCONDITIONDATE (String CONDITIONDATE)
+    {
+        this.CONDITIONDATE = CONDITIONDATE;
+    }
+
+    public String getOPERATIONAL_RESPONSIBILITY ()
+{
+    return OPERATIONAL_RESPONSIBILITY;
+}
+
+    public void setOPERATIONAL_RESPONSIBILITY (String OPERATIONAL_RESPONSIBILITY)
+    {
+        this.OPERATIONAL_RESPONSIBILITY = OPERATIONAL_RESPONSIBILITY;
+    }
+
+    public String getHOUSE_NO ()
+{
+    return HOUSE_NO;
+}
+
+    public void setHOUSE_NO (String HOUSE_NO)
+    {
+        this.HOUSE_NO = HOUSE_NO;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [LOCATION = "+LOCATION+", MOD_DATE = "+MOD_DATE+", WORK_ASSIGNMENT = "+WORK_ASSIGNMENT+", OPERATING_LOCATION = "+OPERATING_LOCATION+", SERIAL_NO = "+SERIAL_NO+", INSERVICE_DATE = "+INSERVICE_DATE+", COMMENTS = "+COMMENTS+", BARCODE = "+BARCODE+", YRS_LIFE_EXPECTANCY = "+YRS_LIFE_EXPECTANCY+", type = "+type+", SERVICE_LEVEL = "+SERVICE_LEVEL+", MAKE = "+MAKE+", SURFACE_TREATMENT = "+SURFACE_TREATMENT+", CONTAINER_TYPE = "+CONTAINER_TYPE+", CRE_DATE = "+CRE_DATE+", STATUS = "+STATUS+", OWNER = "+OWNER+", DESCRIPTION = "+DESCRIPTION+", STATUS_PARKS = "+STATUS_PARKS+", PARK_OWNERSHIP = "+PARK_OWNERSHIP+", FACILITYID = "+FACILITYID+", CONTAINER_NO = "+CONTAINER_NO+", YR = "+YR+", OPERATING_LOCATION_TYPE = "+OPERATING_LOCATION_TYPE+", GIS_ENTRY_STAGE = "+GIS_ENTRY_STAGE+", CONTAINER_TYPE2 = "+CONTAINER_TYPE2+", MAP_PAGE = "+MAP_PAGE+", PARK_LEGACY_ID = "+PARK_LEGACY_ID+", INSERVICE_COST = "+INSERVICE_COST+", LEGACYID = "+LEGACYID+", coordinates = "+coordinates+", WARRANTY_PROVIDER = "+WARRANTY_PROVIDER+", STATUS_DATE = "+STATUS_DATE+", MODEL = "+MODEL+", ROAD_CODE = "+ROAD_CODE+", LIFECYCLE_COST = "+LIFECYCLE_COST+", YTD_COST = "+YTD_COST+", PROJECT_NO = "+PROJECT_NO+", MAINTENANCE_RESPONSIBILITY = "+MAINTENANCE_RESPONSIBILITY+", VENDOR = "+VENDOR+", CONDITION = "+CONDITION+", WARRANTYDATE = "+WARRANTYDATE+", CONDITIONDATE = "+CONDITIONDATE+", OPERATIONAL_RESPONSIBILITY = "+OPERATIONAL_RESPONSIBILITY+", HOUSE_NO = "+HOUSE_NO+"]";
+    }
+}
