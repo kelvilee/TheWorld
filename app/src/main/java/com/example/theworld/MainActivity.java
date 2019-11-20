c2ee3773c815dc84ed45a818f8e86fe27ac0689e
Kelvin Lee <kahokelvinlee@gmail.com>
2019-11-20 00:59:45 -0800

    removed fragment toasts

--- app/src/main/java/com/example/theworld/MainActivity.java
+++ app/src/main/java/com/example/theworld/MainActivity.java
@@ -42,15 +42,12 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_recents:
                         selectedFragment = new HomeFragment();
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_favorites:
                         selectedFragment = new MapsFragment();
-                        Toast.makeText(MainActivity.this, "Map", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_nearby:
                         selectedFragment = new ToolsFragment();
-                        Toast.makeText(MainActivity.this, "Tools", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
